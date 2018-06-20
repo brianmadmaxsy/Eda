b813f6e8d4e74670042507626831ad33b378d939
Lauren Brian <madmax@Admins-iMac.local>
2018-06-20 18:08:05 +0800

    Updating latest imported laravel project, adding database migrations for (commands, orders, responses)

---
+++ eda/database/migrations/2018_06_20_094429_create_commands_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->integer('response_id');
+            $table->unsignedTinyInteger('is_command');
+            $table->unsignedTinyInteger('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commands');
+    }
+}
